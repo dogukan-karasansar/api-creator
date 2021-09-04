83b57dd1ff658f53522f6a25581bafddd604b6aa
karasansar <55123100+dogukan-karasansar@users.noreply.github.com>
2021-09-04 21:18:42 +0300

    products detail api

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -63,7 +63,9 @@ class ProductController extends Controller
      */
     public function show($id)
     {
-        //
+        $product = Product::find($id);
+
+        return response()->json($product);
     }
 
     /**
--- routes/api.php
+++ routes/api.php
@@ -16,12 +16,12 @@ Route::prefix('v1')->group(function () {
         //CATEGORY API
         Route::get('category/{id}', [CategoryController::class, 'getCategory']);
         Route::post('category', [CategoryController::class, 'store']);
-        //PRODUCTS API
-        Route::resource('products', ProductController::class);
         //USERS API
         Route::get('users', [UserController::class, 'getUsers']);
         Route::get('user/{id}', [UserController::class, 'getUser']);
     });
+        //PRODUCTS API
+        Route::resource('products', ProductController::class);
         //AUTH ROUTES
         Route::post('register', [AuthController::class, 'register']);
         Route::post('login', [AuthController::class, 'login']);
