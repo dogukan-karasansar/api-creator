c57cd8cd4502882b19096133bd7cdfb9b26e8f5c
karasansar <55123100+dogukan-karasansar@users.noreply.github.com>
2021-09-06 01:51:44 +0300

    bulma ui entegreted and login page reamstered

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.9.3/css/bulma.min.css">
+    <script type="module" src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"></script>
+    <script nomodule src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js"></script>
+    <title>Api Creator - @yield('title')</title>
+</head>
+
+<body>
+    @yield('content')
+    @yield('script')
+</body>
+
+</html>
+
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,33 +1,69 @@
-<!DOCTYPE html>
-<html lang="en">
+@extends("layouts.app")
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Api Creator Admin Login</title>
-</head>
 
-<body>
-    @if(Session::has('errors'))
-    <h3>{{Session::get('errors')}}</h3>
-    @endif
+@section('title', "Admin Login")
+
+
+
+@section('content')
+<div style="display: flex;
+justify-content: center;
+align-items: center; height:100vh">
+
+    <div style="display: flex; position: absolute; top:0; margin-top: 50px">
+        @if(Session::has('errors'))
+        <div class="notification is-danger">
+            <button class="delete"></button>
+            {{Session::get('errors')}}
+        </div>
+        @endif
+    </div>
 
     <form action="{{route('auth.login')}}" method="POST">
         @csrf
-        <div>
-            <label>E-mail</label>
-            <input type="email" name="email" id="email"/>
+        <div class="field">
+            <label class="label">Email</label>
+            <div class="control has-icons-left has-icons-right">
+                <input class="input" type="email" name="email" id="email" placeholder="Email input" style="width: 300px"
+                    value="test@example.com">
+                <span class="icon is-small is-left">
+                    <ion-icon name="mail" style="font-size: 23px"></ion-icon>
+                </span>
+                <span class="icon is-small is-right">
+                    <ion-icon name="warning" style="font-size: 23px"></ion-icon>
+                </span>
+            </div>
         </div>
-        <br/>
-        <div>
-            <label>Password</label>
-            <input type="password" name="password" id="password"/>
-        </div>
-        <br/>
-        <input type="submit" value="Giriş" name="send"/>
-    </form>
-</body>
 
-</html>
+        <div class="field">
+            <label class="label">Password</label>
+            <p class="control has-icons-left">
+                <input class="input" type="password" name="password" id="password" style="width: 300px"
+                    placeholder="Password">
+                <span class="icon is-small is-left">
+                    <ion-icon name="lock-closed" style="font-size: 23px"></ion-icon>
+                </span>
+            </p>
+        </div>
+
+        <div style="display:flex; justify-content: center">
+            <button type="submit" class="button is-dark">Giriş</button>
+        </div>
+    </form>
+</div>
+@endsection
+
+@section('script')
+<script>
+    document.addEventListener('DOMContentLoaded', () => {
+    (document.querySelectorAll('.notification .delete') || []).forEach(($delete) => {
+      const $notification = $delete.parentNode;
+
+      $delete.addEventListener('click', () => {
+        $notification.parentNode.removeChild($notification);
+      });
+    });
+  });
+</script>
+@endsection
 
